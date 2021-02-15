b0384c460199fd141b089a8e54690ef5d279af7b
Shreyes <shreyessrivastava@gmail.com>
2021-02-15 16:50:46 +0530

    login page

--- app/src/main/java/com/example/login_signup_firebase/MainActivity.java
+++ app/src/main/java/com/example/login_signup_firebase/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.login_signup_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/login_signup_firebase/Register.java
+++ app/src/main/java/com/example/login_signup_firebase/Register.java
@@ -91,5 +91,13 @@ public class Register extends AppCompatActivity {
 
             }
         });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+
+            }
+        });
     }
 }
